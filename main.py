b5b87ffbbd80f6bf8a8b3cb7cf63860c9ab325f6
KAlexRussel <kouawoualex1234@gmail.com>
2024-10-29 16:16:06 +0100

    update

--- main.py
+++ main.py
@@ -10,28 +10,16 @@ import torch
 import scipy.io.wavfile
 import numpy as np
 from IPython.display import Audio
-from pydantic import BaseModel
 
 
-class Item(BaseModel):
-    text: str
 
 app = FastAPI()
-model = VitsModel.from_pretrained("facebook/mms-tts-fon")
-tokenizer = AutoTokenizer.from_pretrained("facebook/mms-tts-fon")
+
 
 @app.get("/")
 def read_root():
     translated = GoogleTranslator(source='fr', target='fon').translate("Bonsoir. Comment vas-tu ?")
     text = translated
-    inputs = tokenizer(text, return_tensors="pt")
-    with torch.no_grad():
-        output = model(**inputs).waveform
-   
-    output = output.cpu()
-    data_np = output.numpy()
-    data_np_squeezed = np.squeeze(data_np)
-    scipy.io.wavfile.write("output.wav", rate=model.config.sampling_rate, data=data_np_squeezed)
     
 
     return {"data": text, "status": 200}
