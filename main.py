ce72e71ce53babbeae8c161941ffc1cbcebeadd2
KAlexRussel <kouawoualex1234@gmail.com>
2024-11-20 14:38:03 +0100

    [ADD]: Post resquest added to translate

--- main.py
+++ main.py
@@ -1,13 +1,14 @@
 from fastapi import FastAPI
 # from flask import Flask
 from deep_translator import GoogleTranslator
-
-
+from pydantic import BaseModel
 
 
 app = FastAPI()
 
-
+class Item(BaseModel):
+    texts: str | None = None
+    
 @app.get("/")
 def read_root():
     translated = GoogleTranslator(source='fr', target='fon').translate("Bonsoir. Comment vas-tu ?")
@@ -25,3 +26,18 @@ def createText(item: str):
     text = translated
 
     return {"data": text, "status": 200}
+
+@app.post("/create_fon/")
+async def create_fon(text_data: Item):
+    print(text_data.texts)
+    untranslated = text_data.texts  
+    translated = GoogleTranslator(source='fr', target='fon').translate(untranslated)
+    text = translated
+
+    return {"data": text, "status": 200}
+
+
+
+if __name__ == '__main__':
+    import uvicorn
+    uvicorn.run(app)
